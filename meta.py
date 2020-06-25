c47a39179c96857d6592d10267d0317d7a5c59e6
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2020-06-25 20:02:29 +0200

    GRRRRR

--- meta.py
+++ meta.py
@@ -1,9 +1,13 @@
 import subprocess
 import os
+import numpy as np
+import pickle
+import subprocess
+from shlex import split
 PARAM = np.linspace(0,1,100)
-for i in range(len(PARAM)//12+1)
+for i in range(len(PARAM)//12+1):
+	print(i)
 	KWARGS = PARAM[12*i:12*(i+1)]
-	os.putenv("KWARGS",KWARGS)
-	os.putenv("i",i)
-	process = subprocess.Popen("srun -N=1 -n=12 -o=~/Results/$i python try.py $KWARGS")
-	process.communicate()
+	pickle.dump(KWARGS,open("./KWARGS_"+str(i),"wb"))
+	bash = "srun -N 1 --output=./Results/" + str(i) +" python try.py "+str(i) + " &"
+	subprocess.Popen(bash.split(),stdout=subprocess.PIPE)	
