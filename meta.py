f28f5e0a457ed97e94785110884a004ec6aefda7
regicid <57593826+regicid@users.noreply.github.com>
2020-06-25 15:24:33 +0200

    Update meta.py

--- meta.py
+++ meta.py
@@ -1,4 +1,9 @@
-os.putenv("KWARGS",PARAM[i:i+12])
-
-
-subprocess.Popen(srun -N=1 -n=12 python try.py $KWARGS)
+import subprocess
+import os
+PARAM = np.linspace(0,1,100)
+for i in range(len(PARAM)//12+1)
+	KWARGS = PARAM[12*i:12*(i+1)]
+	os.putenv("KWARGS",KWARGS)
+	os.putenv("i",i)
+	process = subprocess.Popen("srun -N=1 -n=12 -o=~/Results/$i python try.py $KWARGS")
+	process.communicate()
