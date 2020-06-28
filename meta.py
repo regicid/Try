626aefda42cc4cc5eefedfe4ca74c40137eb1665
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2020-06-28 16:24:21 +0200

    bla

--- meta.py
+++ meta.py
@@ -4,13 +4,14 @@ import numpy as np
 import pickle
 import subprocess
 from shlex import split
+import math
 os.system("mkdir ./Results")
-PARAM = np.linspace(0,10,60)
+PARAM = np.linspace(0,10,80)
 C = [0,.01,.03,.05]
 for c in C:
-	for i in range(len(PARAM)//12+1):
+	for i in range(math.ceil(len(PARAM)/40)):
 		print(c)
-		KWARGS = PARAM[12*i:12*(i+1)]
+		KWARGS = PARAM[40*i:40*(i+1)]
 		pickle.dump(KWARGS,open("./KWARGS_"+str(i),"wb"))
-		bash = "srun -N 1 --partition=firstgen --output=./Results/" + str(i) +" python try.py "+str(i) + " " + str(c)
+		bash = "srun -N 1 --partition=dellgen --output=./Results/" + str(i) +" python try.py "+str(i) + " " + str(c)
 		subprocess.Popen(bash.split(),stdout=subprocess.PIPE)	
