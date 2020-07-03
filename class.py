afd660709d23be2c7f55be33985334957617f675
regicid <57593826+regicid@users.noreply.github.com>
2020-07-03 11:53:41 +0200

    bla

--- class.py
+++ class.py
@@ -1,24 +1,19 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
 import sys
 import multiprocessing as mtp
 
 class CurtyMarsili(object):
-    def __init__(self,z=0,z2 = 0,z3 = 0,a = 1, N=2000, p=0.52, m=11,γ = 0.05,γ2 = .05,σ_mut = 10**-8,α_dandy = 1,n = 100,Ω = 1,c = .03,selection_force=1,raoult=False):
-        #set the parameters
-        self.γ2 = γ2
-        self.raoult = raoult
+    def __init__(self,z=0,z2 = 0,a = 1, N=2000, p=0.52, m=11,γ = 0.1,σ_mut = 10**-8,α_dandy = 1,n = 100,Ω = 1,c = .03,selection_force=1):
         self.N_f = int(np.round(N*z))
+        self.N_α = int(np.round(N*z2))  
         self.N = N
         self.follower = np.zeros(self.N,dtype="bool")
         self.follower[:self.N_f] = True
         self.α = np.zeros(self.N,dtype='bool')
-        self.α[:int(np.round(N*z2))] = True
+        self.α[:self.N_α] = True
         self.anti_conformist = np.zeros(self.N,dtype="bool")
-        b = np.random.random(size=self.N)
-        self.anti_conformist[b<z3] = True
         self.Ω = Ω
         self.p = p
         self.m = m
@@ -32,9 +27,14 @@ class CurtyMarsili(object):
         self.network_scores = np.zeros(shape = (self.N,m))
         for i in range(self.N):
             self.network[i,] = np.random.choice(np.delete(range(N),i),size=m,replace=False)
+        #pick the initial choices for informed and non-informed agents
         self.D = np.empty(N)
         self.D[self.follower] = np.random.choice([-1,1],p = [0.5, 0.5],size = self.follower.sum())
         self.D[~self.follower] = np.random.choice([-1,1],p = [1-p,p],size = N - self.follower.sum())
+        self.α_history = np.zeros(shape=(0,self.N),dtype='bool')
+        self.D_history = np.zeros(shape=(0,self.N),dtype='bool')
+        self.f_history = np.zeros(shape=(0,self.N),dtype='bool')
+        self.fitness_history = np.zeros(shape=(0,3))
         self.q = []
         self.prop_i = []
         self.prop_lazy = []
@@ -44,9 +44,13 @@ class CurtyMarsili(object):
         in_deg[a[0]] = a[1]
         self.in_d = in_deg
         self.N_f = [self.N_f]
-        self.dandy_share = []        
+        self.dandy_share = []
     def compute_q(self):          
         return np.mean(self.D[self.follower] > 0)   
+    def compute_pi(self):
+        return np.mean(self.D > 0)   
+    def compute_informed(self):
+        return np.mean(self.D[~self.follower] > 0)        
     def iterate(self,T=10000): # Iterative imitation process
         self.D[self.follower] = np.random.choice([-1,1],p = [0.5, 0.5],size = self.follower.sum())
         self.D[~self.follower] = np.random.choice([-1,1],p = [1-self.p,self.p],size = self.N - self.follower.sum())
@@ -60,12 +64,13 @@ class CurtyMarsili(object):
                 avg_group_choice = np.mean(group_choices,axis=1)
                 self.D[random_follower] = np.sign(avg_group_choice)*(1-2*self.anti_conformist[random_follower])    
     def dynamics(self,T):
-        self.α_history = np.zeros(shape=(T//100+1,self.N),dtype='bool')
-        self.f_history = np.zeros(shape=(T//100+1,self.N),dtype='bool')
-        self.anti_history = np.zeros(shape=(T//100+1,self.N),dtype='bool')
-        self.accuracy = .5*np.ones(shape=(self.N))
-        self.fitness_history = np.zeros(shape=(T,4))
-        self.q_history = []
+        self.α_temporary = np.zeros(shape=(T,self.N),dtype='bool')
+        self.f_temporary = np.zeros(shape=(T,self.N),dtype='bool')
+        self.D_temporary = np.zeros(shape=(T+1000,self.N),dtype='bool')
+        self.fitness_temporary = np.zeros(shape=(T,3))
+        if len(self.q)>1000:
+            self.D_temporary[:1000,] = self.D_history[-1000:,]
+        self.q_temporary = []
         for t in range(T):
             # Now we update the networks (record scores, and get rid of the worst network member)
             in_deg = np.zeros(self.N,dtype="int")
@@ -84,31 +89,29 @@ class CurtyMarsili(object):
                 p2 = p2/p2.sum()
                 self.network[I,weakest_link[i]] = np.random.choice(not_listened,p = p2)
                 self.network_scores[I,weakest_link[i]] = self.network_scores[I,].mean()
-            b = np.random.random(size=self.N)
-            self.α[b<self.σ_mut] = 1 - self.α[b<self.σ_mut]
-            b = np.random.random(size=self.N)
-            self.follower[b<self.σ_mut] = 1 - self.follower[b<self.σ_mut]
-            if self.raoult:
+            if len(self.q) > 1000 or len(self.q_temporary) > 1000:
+                b = np.random.random(size=self.N)
+                self.α[b<self.σ_mut] = 1 - self.α[b<self.σ_mut]
+                b = np.random.random(size=self.N)
+                self.follower[b<self.σ_mut] = 1 - self.follower[b<self.σ_mut]
                 b = np.random.random(size=self.N)
                 self.anti_conformist[b<self.σ_mut] = 1 - self.anti_conformist[b<self.σ_mut]
-            self.accuracy += self.γ2*(self.D>0) -self.γ2*self.accuracy
-            self.fitness = self.accuracy + self.Ω*in_deg/self.N - self.c*(~self.follower)
-            self.fitness /= self.fitness.sum()
-            for j in range(self.selection_force):
-                self.selection()
-            self.N_f.append(self.follower.sum())
-            if t % 100 == 0:
-                self.α_history[t//100,]= self.α
-                self.f_history[t//100,]= self.follower
-                self.anti_history[t//100,]= self.anti_conformist
-                self.fitness_history[t//100,] = [self.fitness[~self.follower*~self.anti_conformist].mean(),self.fitness[~self.α*self.follower*~self.anti_conformist].mean(),self.fitness[self.α*self.follower*~self.anti_conformist].mean(),self.fitness[self.follower*self.anti_conformist].mean()]
-            self.prop_lazy.append(np.mean((self.follower*~self.α)[self.network])/(self.follower*~self.α).sum())
+                self.fitness = self.D_temporary[t:t+1000,].mean(axis=0) + self.Ω*in_deg/self.N - self.c*(~self.follower)
+                self.fitness /= self.fitness.sum()
+                for j in range(self.selection_force):
+                    self.selection()
+                self.N_f.append(self.follower.sum())
+                self.α_temporary[t,]= self.α
+                self.f_temporary[t,]= self.follower
+                self.fitness_temporary[t,] = [self.fitness[~self.follower].mean(),self.fitness[~self.α*self.follower].mean(),self.fitness[self.α*self.follower].mean()]
+                self.prop_lazy.append(np.mean((self.follower*~self.α)[self.network])/(self.follower*~self.α).sum())
             self.iterate()
-            self.q_history.append(self.compute_q())
+            self.q_temporary.append(self.compute_q())
             self.prop_i.append(1-np.mean(self.follower[self.network]))
             self.dandy_share.append(self.follower[self.network[self.follower*self.α,]].mean())
+            self.D_temporary[1000+t,]= self.D>0
     def selection(self):
-        t = len(self.q_history)
+        t = len(self.q_temporary)
         i = np.random.choice(range(self.N))
         j = np.random.choice(range(self.N),p = self.fitness)
         self.α[i] = self.α[j]
@@ -116,22 +119,11 @@ class CurtyMarsili(object):
         self.network_scores[i,] = self.network_scores[j,]
         self.follower[i] = self.follower[j]
         self.anti_conformist[i] = self.anti_conformist[j]
-        self.accuracy[i] = self.accuracy[j]
-
-i = sys.argv[1]
-c = float(sys.argv[2])
-z = pickle.load(open("KWARGS_"+i,"rb"))
-
-def get_cm(o):
-    CM = CurtyMarsili(z=.04,z2=.02,Ω = o,γ = .05,c = c)
-    CM.dynamics(10**6)
-    o = np.round(o,2)    
-    pickle.dump(CM,open(f"./Results/result_{o}_{c}","wb"))
-
-l = mtp.Pool()
-runs = l.map_async(get_cm,z)
-l.close()
-l.join()
-Results = []
-for run in runs.get():
-    Results.append(run)
+        self.D_temporary[t:t+1000,i] = self.D_temporary[t:t+1000,j]
+    def record(self):
+        t = len(self.q_temporary)
+        F = self.D_temporary[t:t+1000,]
+        self.α_temporary = self.α_temporary[::100,]
+        self.f_temporary = self.f_temporary[::100,]
+        self.fitness_temporary = self.fitness_temporary[::100,]
+        self.q = self.q + self.q_temporary
