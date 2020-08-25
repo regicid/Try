f8df5698323cb82622c4bde49f2b54f9be053653
regicid <57593826+regicid@users.noreply.github.com>
2020-08-25 15:38:27 +0200

    bla

--- compressor.py
+++ compressor.py
@@ -1,13 +1,13 @@
 from classs import CurtyMarsili
-
+import pandas as pd
 def compressor(self):
-    self.f_history = self.f_history[::100]
-    self.α_history = self.α_history[::100]
-    self.prop_i = self.prop_i[::100]
+    self.f_history = pd.DataFrame(self.f_history).rolling(100).mean().values[::100,0]
+    self.α_history = pd.DataFrame(self.α_history).rolling(100).mean().values[::100,0]
+    self.prop_i = pd.DataFrame(self.prop_i).rolling(100).mean().values[::100,0]
     self.q_history = self.q_history[::100]
-    self.anti_history = self.anti_history[::100]
+    self.anti_history = pd.DataFrame(self.anti_history).rolling(100).mean().values[::100,0]
     self.N_f= self.N_f[::100]
-    self.fitness_history = self.fitness_history[::100,]
+    self.fitness_history = pd.DataFrame(self.fitness_history).rolling(100).mean().values[::100,:]
     return r
 
 import os
@@ -18,7 +18,7 @@ for i in a:
         r = pickle.load(open("Results/"+i,"rb"))
         r = compressor(r)
         pickle.dump(r,open(f"./Results_clean/{i}","wb"))
-        os.system("rm Results/"+i)
+        #os.system("rm Results/"+i)
     except:
         print(i)
-        os.system("rm Results/"+i)
+        #os.system("rm Results/"+i)
